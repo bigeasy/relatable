5389fa2b67436a43775b180eb6c2835290e50f89
Alan Gutierrez <alan@prettyrobots.com>
2011-09-21 03:11:16 +0000

    PostgreSQL DDL more SQL like.

--- ddl/postgresql.sql
+++ ddl/postgresql.sql
@@ -6,51 +6,51 @@ CREATE TABLE Manufacturer (
 \g
 CREATE TABLE Customer (
     id                  SERIAL NOT NULL,
-    firstName           VARCHAR(32) NOT NULL,
-    lastName            VARCHAR(32) NOT NULL,
+    first_name          VARCHAR(32) NOT NULL,
+    last_name           VARCHAR(32) NOT NULL,
     PRIMARY KEY(id)
 )
 \g
 CREATE TABLE Sale (
     id                  SERIAL NOT NULL,
-    customerId          INTEGER NOT NULL,
+    customer_id         INTEGER NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY (customerId) REFERENCES Customer(id)
+    FOREIGN KEY (customer_id) REFERENCES Customer(id)
 )
 \g
 CREATE TABLE Product (
     id                  SERIAL NOT NULL,
-    manufacturerId      INTEGER NOT NULL,
-    manufacturerCode    VARCHAR(32) NOT NULL,
+    manufacturer_id     INTEGER NOT NULL,
+    manufacturer_code   VARCHAR(32) NOT NULL,
     name                VARCHAR(32) NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY (manufacturerId) REFERENCES Manufacturer(id)
+    FOREIGN KEY (manufacturer_id) REFERENCES Manufacturer(id)
 )
 \g
-CREATE UNIQUE INDEX legacy_product_id ON Product(manufacturerId, manufacturerCode)
+CREATE UNIQUE INDEX legacy_product_id ON Product(manufacturer_id, manufacturer_code)
 \g
 CREATE INDEX product_name ON Product(name)
 \g
-CREATE TABLE SaleItem (
+CREATE TABLE Sale_Item (
     id                  SERIAL NOT NULL,
-    saleId              INTEGER NOT NULL,
+    sale_id             INTEGER NOT NULL,
     quantity            INTEGER NOT NULL,
     price               DECIMAL(7,2) NOT NULL,
-    manufacturerId      INTEGER NOT NULL,
-    manufacturerCode    VARCHAR(32) NOT NULL,
+    manufacturer_id     INTEGER NOT NULL,
+    manufacturer_code   VARCHAR(32) NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY (saleId) REFERENCES Sale(id),
-    FOREIGN KEY (manufacturerId) REFERENCES Manufacturer(id),
-    FOREIGN KEY (manufacturerId, manufacturerCode) REFERENCES Product(manufacturerId, manufacturerCode)
+    FOREIGN KEY (sale_id) REFERENCES Sale(id),
+    FOREIGN KEY (manufacturer_id) REFERENCES Manufacturer(id),
+    FOREIGN KEY (manufacturer_id, manufacturer_code) REFERENCES Product(manufacturer_id, manufacturer_code)
 )
 \g
 INSERT INTO Manufacturer(name) VALUES('Acme')
 \g
-INSERT INTO Product(name, manufacturerId, manufacturerCode) VALUES('Heavy Anvil', 1, 'A')
+INSERT INTO Product(name, manufacturer_id, manufacturer_code) VALUES('Heavy Anvil', 1, 'A')
 \g
-INSERT INTO Customer(firstName, lastName) VALUES('Wile E.', 'Coyote')
+INSERT INTO Customer(first_name, last_name) VALUES('Wile E.', 'Coyote')
 \g
-INSERT INTO Sale(customerId) VALUES(1)
+INSERT INTO Sale(customer_id) VALUES(1)
 \g
-INSERT INTO SaleItem(saleId, manufacturerId, manufacturerCode, quantity, price) VALUES(1, 1, 'A', 2, 44.99)
+INSERT INTO Sale_Item(sale_id, manufacturer_id, manufacturer_code, quantity, price) VALUES(1, 1, 'A', 2, 44.99)
 \g
