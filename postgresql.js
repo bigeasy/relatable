ed4bdfbbc80d5f3438caf9273b7e0b351dbf1019
Alan Gutierrez <alan@prettyrobots.com>
2013-04-03 00:53:14 -0400

    Reimplement `postgresql.js` using Cadence.

--- postgresql.js
+++ postgresql.js
@@ -1,4 +1,7 @@
-var pg = require("pg"), Mutator = require("./engine").Mutator, __slice = [].slice;
+var pg = require("pg"),
+    Mutator = require("./engine").Mutator,
+    __slice = [].slice,
+    cadence = require("cadence");
 
 pg.defaults.parseFloat = false;
 pg.defaults.hideDeprecationWarnings = true;
@@ -10,24 +13,6 @@ function die () {
 
 function say () { console.log.apply(console, __slice.call(arguments, 0)) }
 
-function validator (callback) {
-  return function (forward) { return check(callback, forward) }
-}
-
-function check (callback, forward) {
-  return function (error) {
-    if (error) {
-      callback(error);
-    } else {
-      try {
-        forward.apply(null, __slice.call(arguments, 1));
-      } catch (error) {
-        callback(error);
-      }
-    }
-  }
-}
-
 exports.Engine = Engine;
 
 Engine.name = 'Engine';
@@ -36,59 +21,56 @@ function Engine(_configuration) {
   this._configuration = _configuration;
 }
 
-Engine.prototype.schema = function(callback) {
-  var engine = this, schema, connection, okay = validator(callback);
-  if (engine._schmea) {
-    callback(null, engine._schema);
-  } else {
-    schema = engine._schema = {};
-    engine._connect(okay(connected));
-  }
+Engine.prototype.schema = cadence(function(step) {
+  var schema, connection;
+  if (this._schema) return this._schema;
+  else step(function () {
+    schema = this._schema = {};
+    this._connect(step());
+  }, function (connection) {
+    step(function () {
+      connection.sql("\
+      SELECT columns.*\n\
+        FROM information_schema.tables AS tables\n\
+        JOIN information_schema.columns AS columns\n\
+          USING (table_catalog, table_schema, table_name)\n\
+       WHERE table_type = 'BASE TABLE'\n\
+         AND tables.table_schema NOT IN ('pg_catalog', 'information_schema')\n\
+        ", step());
 
-  function connected ($connection) {
-    connection = $connection;
-    connection.sql("\
-    SELECT columns.*\n\
-      FROM information_schema.tables AS tables\n\
-      JOIN information_schema.columns AS columns\n\
-        USING (table_catalog, table_schema, table_name)\n\
-     WHERE table_type = 'BASE TABLE'\n\
-       AND tables.table_schema NOT IN ('pg_catalog', 'information_schema')\n\
-      ", okay(columns));
-  }
+    }, function (results) {
 
-  function columns (results) {
-    results.rows.forEach(function (column) {
-      if (!schema[column.table_schema]) {
-        schema[column.table_schema] = {};
-      }
-      if (!schema[column.table_schema][column.table_name]) {
-        schema[column.table_schema][column.table_name] = { columns: [], key: [] };
-      }
-      var table = schema[column.table_schema][column.table_name];
-      table.columns.push(column.column_name);
+      results.rows.forEach(function (column) {
+        if (!schema[column.table_schema]) {
+          schema[column.table_schema] = {};
+        }
+        if (!schema[column.table_schema][column.table_name]) {
+          schema[column.table_schema][column.table_name] = { columns: [], key: [] };
+        }
+        var table = schema[column.table_schema][column.table_name];
+        table.columns.push(column.column_name);
+      });
+      connection.sql("\
+        SELECT kcu.table_schema, kcu.table_name, kcu.column_name\n\
+          FROM information_schema.table_constraints as tc\n\
+          JOIN information_schema.key_column_usage AS kcu\n\
+            USING (constraint_catalog, constraint_schema, constraint_name,\n\
+                   table_name, table_schema, table_catalog)\n\
+         WHERE constraint_type = 'PRIMARY KEY'\n\
+        ", step());
+
+    }, function (results) {
+
+      results.rows.forEach(function (column) {
+        schema[column.table_schema][column.table_name].key.push(column.column_name);
+      });
+      connection.close("ROLLBACK", step());
+
+    }, function () {
+      return schema;
     });
-    connection.sql("\
-      SELECT kcu.table_schema, kcu.table_name, kcu.column_name\n\
-        FROM information_schema.table_constraints as tc\n\
-        JOIN information_schema.key_column_usage AS kcu\n\
-          USING (constraint_catalog, constraint_schema, constraint_name,\n\
-                 table_name, table_schema, table_catalog)\n\
-       WHERE constraint_type = 'PRIMARY KEY'\n\
-      ", okay(keys));
-  }
-
-  function keys (results) {
-    results.rows.forEach(function (column) {
-      schema[column.table_schema][column.table_name].key.push(column.column_name);
-    });
-    connection.close("ROLLBACK", okay(close));
-  }
-
-  function close () {
-    callback(null, schema);
-  }
-};
+  });
+});
 
 Engine.prototype.temporary = function(structure, parameters) {
   var create, drop, sql;
@@ -101,14 +83,14 @@ Engine.prototype.temporary = function(structure, parameters) {
   return [[create, []], [sql, parameters], [drop, []]];
 };
 
-Engine.prototype.connect = function(callback) {
-  var engine = this;
-  engine.schema(check(callback, function (schema) {
-    engine._connect(function(error, connection) {
-      callback(error, schema, connection);
-    });
-  }));
-};
+Engine.prototype.connect = cadence(function (step) {
+  step(function () {
+    this.schema(step());
+    this._connect(step());
+  }, function (schema, connection) {
+    step(null, schema, connection);
+  });
+});
 
 Engine.prototype._connect = function(callback) {
   var client,
@@ -116,12 +98,12 @@ Engine.prototype._connect = function(callback) {
   this._configuration.database = this._configuration.name;
   client = new pg.Client(this._configuration);
   client.on("connect", function() {
-    return callback(null, new Connection(client));
+    callback(null, new Connection(client));
   });
   client.on("error", function(error) {
-    return callback(error);
+    callback(error);
   });
-  return client.connect();
+  client.connect();
 };
 
 Connection.name = 'Connection';
@@ -139,10 +121,10 @@ Connection.prototype.sql = function(query, parameters, callback) {
 Connection.prototype.close = function(terminator, callback) {
   var connection = this;
   connection._client.once("drain", function() {
-    return callback();
+    callback();
   });
-  return connection._client.query(terminator, [], function() {
-    return connection._client.end();
+  connection._client.query(terminator, [], function() {
+    connection._client.end();
   });
 };
 
